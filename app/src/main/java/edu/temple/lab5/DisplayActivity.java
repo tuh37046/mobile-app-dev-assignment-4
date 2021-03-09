c86289fb30385b2b3650dcfebf554f40564e56c2
garrbows <gbowser3@gmail.com>
2021-03-09 13:28:35 -0500

    initial commit

---
+++ app/src/main/java/edu/temple/lab5/DisplayActivity.java
@@ -0,0 +1,45 @@
+package edu.temple.lab5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class DisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.imageView);
+        TextView  textView = findViewById(R.id.textView);
+
+        ArrayList options = new ArrayList<String>();
+        int[] images = new int[]{R.drawable.apple,R.drawable.banana,R.drawable.blueberry,R.drawable.raspberry};
+        int index;
+
+        Intent data = getIntent();
+
+        options = (ArrayList<String>)data.getSerializableExtra("OPTIONS");
+        images = data.getIntArrayExtra("IMAGES");
+        index = data.getIntExtra("INDEX",0);
+
+
+        textView.setText((String)options.get(index));
+        imageView.setImageResource(images[index]);
+
+    }
+
+}
---
+++ app/src/main/java/edu/temple/lab5/SelectionActivity.java
@@ -0,0 +1,70 @@
+package edu.temple.lab5;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.util.Pair;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class SelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList options = new ArrayList<String>();
+        options.add("Apple");
+        options.add("Banana");
+        options.add("Blueberry");
+        options.add("Raspberry");
+
+        int[] images = new int[]{R.drawable.apple,R.drawable.banana,R.drawable.blueberry,R.drawable.raspberry};
+
+        ImageAdapter adapter = new ImageAdapter(this, options);
+
+       ImageView imageView = findViewById(R.id.imageView);
+       Spinner spinner = findViewById(R.id.spinner);
+       TextView text = findViewById(R.id.textView);
+       spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(SelectionActivity.this, "Item Selected", Toast.LENGTH_SHORT).show();
+                text.setText("Selected "+options.get(position));
+                imageView.setImageResource(images[position]);
+
+                Intent display = new Intent(SelectionActivity.this,DisplayActivity.class);
+                display.putExtra("OPTIONS",options);
+                display.putExtra("IMAGES",images);
+                display.putExtra("INDEX",position);
+
+               startActivity(display);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(SelectionActivity.this, "Deselected item", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+}
